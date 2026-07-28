39385b646c8bec9c0144650f78bb5ca91ff1e773
ANNAMDEVULA DURGA PRASAD <durgaprasadannamdevula41@gmail.com>
2026-07-28 16:43:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 import plotly.graph_objects as go
-
 from mlxtend.frequent_patterns import apriori, association_rules
 
 # ============================
