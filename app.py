298c50e094019bb051a9a444456fb6cadf9e950c
ANNAMDEVULA DURGA PRASAD <durgaprasadannamdevula41@gmail.com>
2026-07-28 16:35:38 +0530

    Remove unused import for plotly.express

--- app.py
+++ app.py
@@ -4,7 +4,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go
 
 from mlxtend.frequent_patterns import apriori, association_rules
